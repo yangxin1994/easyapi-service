8e456d67714b15f00e132f61bac559a3ad392e09
leida <zhang20084@126.com>
2020-10-27 11:22:51 +0800

    fix:登录地址增加from参数

--- src/api/request.js
+++ src/api/request.js
@@ -7,7 +7,7 @@ axios.interceptors.request.use(
   function(config) {
     if (config.url.indexOf("/page/team") == -1) {
       if (!Cookies.get("authenticationToken")) {
-        location.href = "https://account.easyapi.com/login"; // 如果没有authenticationToken存在
+        location.href = "https://account.easyapi.com/login?from=https://service.easyapi.com"; // 如果没有authenticationToken存在
       }
     }
     config.headers.authorization = "Bearer " + Cookies.get("authenticationToken");
@@ -30,7 +30,7 @@ axios.interceptors.response.use(
     // 对响应错误做点什么
     if (error.response.data.code === -9) {
       // 处理-9用户信息不存在
-      window.location.href = "https://account.easyapi.com/login";
+      window.location.href = "https://account.easyapi.com/login?from=https://service.easyapi.com";
     } else if (error.response.data.code === -8) {
       // 处理-8无团队信息
       window.location.href = "https://team.easyapi.com";
