cf165e2b62273dd4775b3784301d29dad106276b
chenyun123 <1537299563@qq.com>
2020-10-29 20:37:09 +0800

    按次的加上提醒上限设置

--- src/pages/setting/balance.vue
+++ src/pages/setting/balance.vue
@@ -8,44 +8,72 @@
         <p v-if="judgmentUnit === 3">
           <span>当前剩余时间：&nbsp;&nbsp;&nbsp;&nbsp;{{ remainDay }}天</span>
         </p>
-        <p style="padding-left:48px">
+        <p style="padding-left: 48px">
           <span>是否提醒：</span>
-          <i-switch size="large" v-model="switch1" @on-change="change" style="margin-left:14px;">
+          <i-switch
+            size="large"
+            v-model="switch1"
+            @on-change="change"
+            style="margin-left: 14px"
+          >
             <span slot="open" style="color: #fff">开启</span>
             <span slot="close" style="color: #fff">关闭</span>
           </i-switch>
         </p>
-        <p v-if="this.type == 2 && this.type == 1 && this.type == 4">
+        <p v-if="type == 2 || type == 1 || type == 4">
           <span>剩余提醒次数：&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <InputNumber v-model="balanceWarnNo" :disabled="!switch1" :min="1"></InputNumber>
+          <InputNumber
+            v-model="balanceWarnNo"
+            :disabled="!switch1"
+            :min="1"
+          ></InputNumber>
           <span>&nbsp;&nbsp;次</span>
         </p>
-        <p v-if="this.type == 3">
+        <p v-if="type == 3">
           <span>剩余提醒时间：&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <InputNumber v-model="balanceWarnNo" :disabled="!switch1" :min="1"></InputNumber>
+          <InputNumber
+            v-model="balanceWarnNo"
+            :disabled="!switch1"
+            :min="1"
+          ></InputNumber>
           <span>&nbsp;&nbsp;天</span>
         </p>
-        <div style="padding-left:48px;display:flex">
-          <span style="line-height:60px;color:#000;font-size: 14px">通知人员：</span>
-          <span style="margin-left:20px;height:auto;width:80%;">
+        <div style="padding-left: 48px; display: flex">
+          <span style="line-height: 60px; color: #000; font-size: 14px"
+            >通知人员：</span
+          >
+          <span style="margin-left: 20px; height: auto; width: 80%">
             <CheckboxGroup
               v-model="checkbox"
-              style="width:100%;height:auto;line-height:60px;;float: left"
+              style="width: 100%; height: auto; line-height: 60px; float: left"
             >
               <Checkbox
                 :label="User.user.id"
                 v-for="(User, userIndex) in tipsMember"
                 :key="userIndex"
-                style="width:150px;height:60px;margin-right:20px;position:relative"
+                style="
+                  width: 150px;
+                  height: 60px;
+                  margin-right: 20px;
+                  position: relative;
+                "
               >
                 <img
                   :src="User.user.photo"
                   alt
-                  style="width:40px;height:40px;border-radius:25px;margin-left:5px;position:absolute;bottom:10px"
+                  style="
+                    width: 40px;
+                    height: 40px;
+                    border-radius: 25px;
+                    margin-left: 5px;
+                    position: absolute;
+                    bottom: 10px;
+                  "
                 />
                 <span
-                  style="margin-left:53px;color: #333333;font-size: 14px;"
-                >{{ User.user.nickname }}</span>
+                  style="margin-left: 53px; color: #333333; font-size: 14px"
+                  >{{ User.user.nickname }}</span
+                >
               </Checkbox>
             </CheckboxGroup>
           </span>
@@ -54,16 +82,31 @@
       <div class="balance_btn">
         <Button
           @click="renew"
-          style="	width:100px;height:40px;font-size:14px;background-color: #18c1d6;color: #fff;margin-top:40px"
-        >续费
+          style="
+            width: 100px;
+            height: 40px;
+            font-size: 14px;
+            background-color: #18c1d6;
+            color: #fff;
+            margin-top: 40px;
+          "
+          >续费
         </Button>
       </div>
     </div>
     <div class="btn">
       <Button
         @click="updateBalanceRemind"
-        style="	width:100px;height:40px;font-size:14px;background-color: #18c1d6;color: #fff;margin-left:120px;margin-top:10px"
-      >保存设置
+        style="
+          width: 100px;
+          height: 40px;
+          font-size: 14px;
+          background-color: #18c1d6;
+          color: #fff;
+          margin-left: 120px;
+          margin-top: 10px;
+        "
+        >保存设置
       </Button>
     </div>
     <div class="ea-warnBox">
@@ -78,42 +121,44 @@
 </template>
 
 <script>
-  import {
-    updateBalanceRemind, getBalanceRemind, getBalanceRemindList
-  } from "../../api/api";
-  import {
-    getServiceUserList
-  } from "../../api/user-service";
+import {
+  updateBalanceRemind,
+  getBalanceRemind,
+  getBalanceRemindList,
+} from "../../api/api";
+import { getServiceUserList } from "../../api/user-service";
 
-  export default {
-    name: "SettingBalance",
-    props: ["judgmentUnit", "balance", "remainDay", "type", "serviceId", "name"],
-    data() {
-      return {
-        switch1: "", //提醒开关
-        balanceWarnNo: 0,
-        checkbox: [],
-        tipsMember: []
-      };
+export default {
+  name: "SettingBalance",
+  props: ["judgmentUnit", "balance", "remainDay", "type", "serviceId", "name"],
+  data() {
+    return {
+      switch1: "", //提醒开关
+      balanceWarnNo: 0,
+      checkbox: [],
+      tipsMember: [],
+    };
+  },
+  methods: {
+    change(status) {
+      if (status) {
+        this.$Message.info("开启提醒");
+      } else {
+        this.$Message.info("取消提醒");
+      }
     },
-    methods: {
-      change(status) {
-        if (status) {
-          this.$Message.info("开启提醒");
-        } else {
-          this.$Message.info("取消提醒");
-        }
-      },
-      //余额提醒上限
-      getBalanceRemind() {
-        getBalanceRemind(this.serviceId).then(res => {
+    //余额提醒上限
+    getBalanceRemind() {
+      getBalanceRemind(this.serviceId)
+        .then((res) => {
           if (res.data.code === 0) {
             this.switch1 = false;
           } else {
             this.switch1 = true;
             this.balanceWarnNo = res.data.content.count;
           }
-        }).catch(error => {
+        })
+        .catch((error) => {
           console.log(error);
           if (error.data.code === -1) {
             this.$Message.warning(error.data.message);
@@ -121,28 +166,31 @@
             this.$Message.error("数据错误");
           }
         });
-      },
-      //修改余额提醒设置
-      updateBalanceRemind() {
-        let data = {
-          remindUserIds: this.checkbox.join(","),
-          ifRemind: this.switch1,
-          serviceId: this.serviceId,
-          count: this.balanceWarnNo
-        };
-        updateBalanceRemind(data).then(res => {
+    },
+    //修改余额提醒设置
+    updateBalanceRemind() {
+      let data = {
+        remindUserIds: this.checkbox.join(","),
+        ifRemind: this.switch1,
+        serviceId: this.serviceId,
+        count: this.balanceWarnNo,
+      };
+      updateBalanceRemind(data)
+        .then((res) => {
           this.$Message.success(res.data.message);
-        }).catch(error => {
+        })
+        .catch((error) => {
           console.log(error);
           this.$Message.error(error.response.data.message);
         });
-      },
-      //提示人员
-      getBalanceRemindList() {
-        let params = {
-          serviceId: this.serviceId
-        };
-        getBalanceRemindList(params).then(res => {
+    },
+    //提示人员
+    getBalanceRemindList() {
+      let params = {
+        serviceId: this.serviceId,
+      };
+      getBalanceRemindList(params)
+        .then((res) => {
           this.selectedPersonnel = res.data.content;
           let code = res.data.code;
           if (code !== 0) {
@@ -150,86 +198,92 @@
               this.checkbox[i] = this.selectedPersonnel[i].remindUser.id;
             }
           }
-        }).catch(error => {
+        })
+        .catch((error) => {
           console.log(error);
         });
-      },
-      //余额提醒成员列表
-      getServiceUserList() {
-        let params = {
-          size: 100,
-          types: "创建人,管理员"
-        };
-        getServiceUserList(this.serviceId, params).then(res => {
-          this.tipsMember = res.data.content;
-        }).catch(error => {
-          console.log(error);
-        });
-      },
-      //续费跳转页面
-      renew() {
-        let num;
-        if (this.type == 2) {
-          num = this.balance;
-        } else if (this.type == 3) {
-          num = this.remainDay;
-        }
-        let url = `https://team.easyapi.com/service/pay?type=${this.type}&serviceId=${this.serviceId}&serviceName=${this.name}&num=${num}`;
-        let a = document.createElement("a");
-        a.href = url;
-        a.target = "_blank";
-        a.click();
-      }
     },
-    created() {
-      this.getBalanceRemind();
-      this.getBalanceRemindList();
-      this.getServiceUserList();
-    }
-  };
+    //余额提醒成员列表
+    getServiceUserList() {
+      let params = {
+        size: 100,
+        types: "创建人,管理员",
+      };
+      getServiceUserList(this.serviceId, params)
+        .then((res) => {
+          this.tipsMember = res.data.content;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    //续费跳转页面
+    renew() {
+      let num;
+      if (this.type == 2) {
+        num = this.balance;
+        console.log(this.balance);
+      } else if (this.type == 3) {
+        num = this.remainDay;
+        console.log(this.remainDay);
+      }
+      // let url = `https://team.easyapi.com/service/pay?type=${this.type}&serviceId=${this.serviceId}&serviceName=${this.name}&num=${num}`;
+      let url = `http://localhost:8081/service/pay?type=${this.type}&serviceId=${this.serviceId}&serviceName=${this.name}&num=${num}`;
+      let a = document.createElement("a");
+      a.href = url;
+      a.target = "_blank";
+      a.click();
+    },
+  },
+  created() {
+    this.getBalanceRemind();
+    this.getBalanceRemindList();
+    this.getServiceUserList();
+  },
+};
 </script>
 <style lang="stylus">
-  .secret_balance {
-    width: 100%;
-    height: auto;
-    display: flex;
-  }
+.secret_balance {
+  width: 100%;
+  height: auto;
+  display: flex;
+}
 
-  .secret_balance .balance_remind {
-    width: 80%;
-    height: auto;
-  }
+.secret_balance .balance_remind {
+  width: 80%;
+  height: auto;
+}
 
-  .secret_balance .balance_remind p {
-    width: 100%;
-    height: 50px;
-    line-height: 50px;
-    padding-left: 20px;
-  }
+.secret_balance .balance_remind p {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding-left: 20px;
+}
 
-  .secret_balance .balance_remind p span {
-    color: #000000;
-    font-size: 14px;
-  }
+.secret_balance .balance_remind p span {
+  color: #000000;
+  font-size: 14px;
+}
 
-  .balance_btn {
-    width: 20%;
-    height: auto;
-  }
+.balance_btn {
+  width: 20%;
+  height: auto;
+}
 
-  .btn {
-    width: 100%;
-    height: 60px;
-  }
+.btn {
+  width: 100%;
+  height: 60px;
+}
 
-  .ea-warnBox {
-    width: 100%;
-    height: auto;
-    padding-left: 130px;
-  }
+.ea-warnBox {
+  width: 100%;
+  height: auto;
+  padding-left: 130px;
+}
 
-  .ea-warnBox p {
-    color: #999999;
-    font-size: 14px;
-  }
+.ea-warnBox p {
+  color: #999999;
+  font-size: 14px;
+}
 </style>
--- src/pages/setting/secret.vue
+++ src/pages/setting/secret.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="secret_key">
-    <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
-      <FormItem label="appKey" prop="appKey" style="margin-top:20px;" class="fk">
+    <Form
+      ref="formValidate"
+      :model="formValidate"
+      :rules="ruleValidate"
+      :label-width="100"
+    >
+      <FormItem
+        label="appKey"
+        prop="appKey"
+        style="margin-top: 20px"
+        class="fk"
+      >
         <Input
           placeholder="appKey"
           v-model="formValidate.appKey"
           disabled
-          style="width: 300px;"
+          style="width: 300px"
           id="copyAppKey"
         />
         <button
@@ -14,7 +24,8 @@
           class="copy"
           :data-clipboard-text="this.formValidate.appKey"
           @click="copyAppKey"
-        >复制
+        >
+          复制
         </button>
       </FormItem>
       <FormItem label="appSecret" prop="appSecret" class="fk">
@@ -23,7 +34,7 @@
           v-model="formValidate.appSecret"
           disabled
           placeholder="appSecret"
-          style="width:300px"
+          style="width: 300px"
           class="copyContent"
         />
         <button
@@ -31,18 +42,25 @@
           class="copy"
           :data-clipboard-text="this.formValidate.appSecret"
           @click="copyAppSecret"
-        >复制
+        >
+          复制
         </button>
         <span class="display" @click="showInputData">{{ btnContent }}</span>
       </FormItem>
       <FormItem>
-        <Button
-          type="info"
-          @click="showResetKey()"
-        >更换秘钥
-        </Button>
-        <span style="display: block;margin-top:10px;color: #999999;font-size: 12px">此秘钥仅限于短信服务使用</span>
-        <span style="line-height:0px;color: #999999;font-size: 12px">出于安全考虑，建议您周期性的更换密钥</span>
+        <Button type="info" @click="showResetKey()">更换秘钥 </Button>
+        <span
+          style="
+            display: block;
+            margin-top: 10px;
+            color: #999999;
+            font-size: 12px;
+          "
+          >此秘钥仅限于短信服务使用</span
+        >
+        <span style="line-height: 0px; color: #999999; font-size: 12px"
+          >出于安全考虑，建议您周期性的更换密钥</span
+        >
       </FormItem>
     </Form>
 
@@ -55,7 +73,9 @@
       width="350"
     >
       <div>
-        <p>重置之后，以前的appKey和appSecret将不能正常使用，并且会导致以前的API调用失效。重置appKey和appSecret后，请及时将以前的接口调用中的参数进行更新。</p>
+        <p>
+          重置之后，以前的appKey和appSecret将不能正常使用，并且会导致以前的API调用失效。重置appKey和appSecret后，请及时将以前的接口调用中的参数进行更新。
+        </p>
         <p>确认重置appKey和appSecret吗？</p>
       </div>
     </Modal>
@@ -63,142 +83,144 @@
 </template>
 
 <script>
-  import Clipboard from "clipboard";
-  import {
-    resetKey, getTeamService
-  } from "../../api/team-service";
+import Clipboard from "clipboard";
+import { resetKey, getTeamService } from "../../api/team-service";
 
-  export default {
-    name: "SettingSecret",
-    props: ["teamServiceId"],
-    data() {
-      return {
-        ruleValidate: {
-          appKey: [
-            {
-              required: true,
-              message: "请输入appKey，且不能为空",
-              trigger: "blur"
-            }
-          ],
-          appSecret: [
-            {
-              required: true,
-              message: "请输入appSecret，且不能为空",
-              trigger: "blur"
-            }
-          ]
-        },
-        formValidate: {
-          appKey: "",
-          appSecret: ""
-        },
-        inputType: "password",
-        btnContent: "显示",
-        resetKeyModal: false
-      };
+export default {
+  name: "SettingSecret",
+  props: ["teamServiceId"],
+  data() {
+    return {
+      ruleValidate: {
+        appKey: [
+          {
+            required: true,
+            message: "请输入appKey，且不能为空",
+            trigger: "blur",
+          },
+        ],
+        appSecret: [
+          {
+            required: true,
+            message: "请输入appSecret，且不能为空",
+            trigger: "blur",
+          },
+        ],
+      },
+      formValidate: {
+        appKey: "",
+        appSecret: "",
+      },
+      inputType: "password",
+      btnContent: "显示",
+      resetKeyModal: false,
+    };
+  },
+  methods: {
+    copyAppKey() {
+      let clipboard = new Clipboard(".copy");
+      clipboard.on("success", () => {
+        this.$Message.success("复制appKey成功");
+        // 释放内存
+        clipboard.destroy();
+      });
+      clipboard.on("error", () => {
+        // 不支持复制
+        this.$Message.error("该浏览器不支持自动复制");
+        // 释放内存
+        clipboard.destroy();
+      });
     },
-    methods: {
-      copyAppKey() {
-        let clipboard = new Clipboard(".copy");
-        clipboard.on("success", () => {
-          this.$Message.success("复制appKey成功");
-          // 释放内存
-          clipboard.destroy();
-        });
-        clipboard.on("error", () => {
-          // 不支持复制
-          this.$Message.error("该浏览器不支持自动复制");
-          // 释放内存
-          clipboard.destroy();
-        });
-      },
-      copyAppSecret() {
-        let clipboard = new Clipboard(".copy");
-        clipboard.on("success", () => {
-          this.$Message.success("复制appSecret成功");
-          // 释放内存
-          clipboard.destroy();
-        });
-        clipboard.on("error", () => {
-          // 不支持复制
-          this.$Message.error("该浏览器不支持自动复制");
-          // 释放内存
-          clipboard.destroy();
-        });
-      },
-      showInputData() {
-        if (this.inputType === "password") {
-          this.inputType = "text";
-          this.btnContent = "隐藏";
-        } else {
-          this.inputType = "password";
-          this.btnContent = "显示";
-        }
-      },
-      //获取秘钥信息
-      secretKey() {
-        getTeamService(this.teamServiceId).then(res => {
+    copyAppSecret() {
+      let clipboard = new Clipboard(".copy");
+      clipboard.on("success", () => {
+        this.$Message.success("复制appSecret成功");
+        // 释放内存
+        clipboard.destroy();
+      });
+      clipboard.on("error", () => {
+        // 不支持复制
+        this.$Message.error("该浏览器不支持自动复制");
+        // 释放内存
+        clipboard.destroy();
+      });
+    },
+    showInputData() {
+      if (this.inputType === "password") {
+        this.inputType = "text";
+        this.btnContent = "隐藏";
+      } else {
+        this.inputType = "password";
+        this.btnContent = "显示";
+      }
+    },
+    //获取秘钥信息
+    secretKey() {
+      getTeamService(this.teamServiceId)
+        .then((res) => {
           this.formValidate.appKey = res.data.appKey;
           this.formValidate.appSecret = res.data.appSecret;
-        }).catch(error => {
+        })
+        .catch((error) => {
           console.log(error);
         });
-      },
-      ok() {
-        resetKey(this.teamServiceId).then(res => {
+    },
+    ok() {
+      resetKey(this.teamServiceId)
+        .then((res) => {
           this.formValidate.appKey = res.data.content.appKey;
           this.formValidate.appSecret = res.data.content.appSecret;
           this.$Message.success(res.data.message);
-        }).catch(error => {
+        })
+        .catch((error) => {
           console.log(error);
           this.$Message.error(error.data.message);
         });
-      },
-      cancel() {
-        this.resetKeyModal = false;
-      },
-      //修改秘钥
-      showResetKey() {
-        this.resetKeyModal = true;
-      }
     },
-    created() {
-      this.secretKey();
-    }
-  };
+    cancel() {
+      this.resetKeyModal = false;
+    },
+    //修改秘钥
+    showResetKey() {
+      this.resetKeyModal = true;
+    },
+  },
+  mounted() {
+   this.secretKey();
+  },
+};
 </script>
 <style lang="stylus">
-  .fk .ivu-form-item-content {
-    display: flex;
-  }
+.fk .ivu-form-item-content {
+  display: flex;
+}
 
-  .copy {
-    display: block;
-    width: 50px;
-    height: 32px;
-    text-align: center;
-    line-height: 30px;
-    border: 1px solid #dcdee2;
-    background-color: #f5f5f5;
-    border-bottom-right-radius: 4px;
-    border-top-right-radius: 4px;
-    cursor: pointer;
-  }
+.copy {
+  display: block;
+  width: 50px;
+  height: 32px;
+  text-align: center;
+  line-height: 30px;
+  border: 1px solid #dcdee2;
+  background-color: #f5f5f5;
+  border-bottom-right-radius: 4px;
+  border-top-right-radius: 4px;
+  cursor: pointer;
+}
 
-  .display {
-    width: 32px;
-    height: 18px;
-    line-height: 18px;
-    text-align: center;
-    background-color: #4a91f8;
-    border-radius: 3px;
-    border: solid 1px #427fd8;
-    color: #ffffff;
-    font-size: 12px;
-    cursor: pointer;
-    position: absolute;
-    left: 250px;
-    top: 8px;
-  }
+.display {
+  width: 32px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  background-color: #4a91f8;
+  border-radius: 3px;
+  border: solid 1px #427fd8;
+  color: #ffffff;
+  font-size: 12px;
+  cursor: pointer;
+  position: absolute;
+  left: 250px;
+  top: 8px;
+}
 </style>
